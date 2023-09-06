3e605586c80e9f08bd191a5a5652013cfb7e1915
Ammar Tutunchy <amamr1intheair@gmail.com>
2023-09-06 12:52:37 +0300

    the first program in the Java

--- squirrels-nuts.java
+++ squirrels-nuts.java
@@ -1,1 +1,15 @@
 import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    // put your code here
+    int a=0;
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    if (n<10000&& n>0){
+    if (k<10000 && k>0)
+    a=k/n;
+   System.out.println(a);
+  }
+  }
+}
